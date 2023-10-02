cc0df1fa0a3ecc8aee9d87285109a05e0e30a3ad
Nick H. <113550184+NicKreb@users.noreply.github.com>
2023-10-02 21:38:12 +0000

    modified:   build/asset-manifest.json

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={ theme }>
-      <CssBaseline /> 
+      <CssBaseline />
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
